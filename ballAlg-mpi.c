f21747474d69a7aea2e6706c68411571c3738d0d
José Martinho <jose.m.olieiramartinho@gmail.com>
2021-05-29 20:07:47 +0100

    finished proj table

--- ballAlg-mpi.c
+++ ballAlg-mpi.c
@@ -353,7 +353,7 @@ struct node* build_tree(long node_index, long* current_set, long current_set_siz
 
         center = (double*)malloc(dim*sizeof(double));
         for (int d = 0; d < dim; d++) {
-            center[d] = (a[d] + b[d])/2
+            center[d] = (points[a][d] + points[b][d])/2;
         }
 
 
